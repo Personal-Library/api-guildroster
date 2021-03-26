b2f3fc6d346d6771203c741ce8f9c486951dbc25
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-26 16:21:24 -0500

    feat: Basic CRUD routes added for members

--- queries/membersQueries.js
+++ queries/membersQueries.js
@@ -2,3 +2,43 @@
  * 'members' is pluralized to match table name, any queries on the members
  * table will go below
  */
+
+const pool = require('../db');
+
+const getMembers = async () => {
+	const { rows } = await pool.query('SELECT * FROM members;');
+	return rows;
+};
+
+const getOneMember = async (id) => {
+	const { rows } = await pool.query('SELECT * FROM members WHERE id = $1;', [id]);
+	return rows;
+};
+
+// Note that 'class' is a reserved variable in JavaScript
+const createMember = async (username, rank, race, classname) => {
+	const {
+		rows,
+	} = await pool.query(
+		'INSERT INTO members (username, rank, race, class) VALUES ($1, $2, $3, $4) RETURNING *;',
+		[username, rank, race, classname]
+	);
+	return rows;
+};
+
+const deleteMember = async (id) => {
+	const { rows } = await pool.query('DELETE FROM members WHERE id = $1 RETURNING *;', [id]);
+	return rows;
+};
+
+const updateMember = async (username, rank, race, classname, id) => {
+	const {
+		rows,
+	} = await pool.query(
+		'UPDATE members SET username = $1, rank = $2, race = $3, class = $4 WHERE id = $5 RETURNING *;',
+		[username, rank, race, classname, id]
+	);
+	return rows;
+};
+
+module.exports = { getMembers, getOneMember, createMember, deleteMember, updateMember };
--- routes/members.js
+++ routes/members.js
@@ -1,9 +1,57 @@
 const express = require('express');
+const { getMembers, getOneMember, createMember, deleteMember, updateMember } = require('../queries/membersQueries');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send({ msg: 'Member router success!' });
+router.get('/', async (req, res) => {
+	try {
+		const data = await getMembers();
+		res.send(data);
+	} catch (error) {
+		console.error(error);
+	}
 });
+
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const data = await getOneMember(id);
+		res.send(data);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+// 'classname' must be on the request body, not 'class'!
+router.post('/', async (req, res) => {
+	const { username, rank, race, classname } = req.body;
+	try {
+		const data = await createMember(username, rank, race, classname);
+		res.send(data);
+	} catch (error) {
+		console.error(error)
+	}
+})
+
+router.delete('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const data = await deleteMember(id);
+		res.send({ msg: "Member successfully deleted.", data })
+	} catch (error) {
+		console.error(error)
+	}	
+})
+
+router.put('/:id', async (req, res) => {
+	const id = req.params.id;
+	const { username, rank, race, classname } = req.body;
+	try {
+		const data = await updateMember(username, rank, race, classname, id)
+		res.send({ msg: "Member successfully updated.", data})
+	} catch (error) {
+		console.error(error)
+	}
+})
 
 module.exports = router;
